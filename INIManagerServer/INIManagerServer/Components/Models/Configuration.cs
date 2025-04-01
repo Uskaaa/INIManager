0cc8612d371a5845995ef0582f944804884b5a6e
Bene <benedikt.haller@muehlbauer.de>
2025-04-01 16:19:21 +0200

    .

--- INIManagerServer/INIManagerServer/Components/Models/Configuration.cs
+++ INIManagerServer/INIManagerServer/Components/Models/Configuration.cs
@@ -5,6 +5,6 @@ public class Configuration
     public int Id { get; set; }
     public string Name { get; set; }
     public string Path { get; set; }
-    public int AmountOfWS { get; set; }
+    public List<Workstation> AmountOfWS { get; set; }
     public string CreationDate { get; set; }
 }
